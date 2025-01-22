af6f1617e71fe1a5a4cfba277d4ad0bffe95981f
GeorgeMolala <GeorgeMolala@outlook.com>
2025-01-23 01:14:54 +0200

    Add project files.

---
+++ MagicSite/Models/Prod_ImageTbl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagicSite.Models
+{
+    public class Prod_ImageTbl
+    {
+    }
+}
