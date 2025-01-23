af34c89eba29b517f6635e357031d1391d9f07df
GeorgeMolala <GeorgeMolala@outlook.com>
2025-01-24 00:11:27 +0200

    Product Models completed

--- MagicSite/Models/CategoryTbl.cs
+++ MagicSite/Models/CategoryTbl.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace MagicSite.Models
 {
     public class CategoryTbl
     {
+        [Key]
+        public int Cat_ID { get; set; }
+
+        public string CatName { get; set; }
+
+        public string CatDescription { get; set; }
+
     }
 }
