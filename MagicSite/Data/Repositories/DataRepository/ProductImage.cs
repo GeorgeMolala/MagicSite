41c091ec3f90dd3034e5e8f1a7465b4f4a1e3889
GeorgeMolala <GeorgeMolala@outlook.com>
2025-02-03 23:19:33 +0200

    V2Not Complete

--- MagicSite/Data/Repositories/DataRepository/ProductImage.cs
+++ MagicSite/Data/Repositories/DataRepository/ProductImage.cs
@@ -1,17 +1,40 @@
-﻿using MagicSite.Data.Repositories.Helper_Classes;
+﻿using Dapper;
+using MagicSite.Data.Repositories.Helper_Classes;
 using MagicSite.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace MagicSite.Data.Repositories.DataRepository
 {
     public class ProductImage : IProdImage
     {
+        private readonly DataBaseConnection _con;
+
+        public ProductImage(DataBaseConnection con)
+        {
+            _con = con;
+        }
+
         public Task<int> Add(Prod_ImageTbl entity)
         {
-            throw new NotImplementedException();
+           var StoredProcedure = "AddProductImage_PDO";
+
+            using(var connection = _con.CreateConnection())
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("Prod_Image_ID", entity.Prod_Image_ID, DbType.Int32);
+                parameters.Add("Prod_ID", entity.Prod_ID, DbType.Int32);
+                parameters.Add("ImageName", entity.ImageName, DbType.String);
+                parameters.Add("Image_Url", entity.Image_Url, DbType.String);
+
+
+                var resp = connection.ExecuteAsync(StoredProcedure, parameters, commandType:CommandType.StoredProcedure);
+
+                return resp;
+            }
         }
 
      
